b84157da861123834fcf4e7b425ec599a542762d
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-27 13:50:34 +0100

    Added truncate logic validation

--- js/app.js
+++ js/app.js
@@ -4,10 +4,18 @@ window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function truncate(string, max) {
-		if (string.length > max) {
+
+		if (string === "" || max === "") {
+			return "Enter values in both input fields";
+		} else if (isNaN(max)) {
+			return "The max value must be a number";
+		} else if (Number(max) % 1 !== 0) {
+			return "The number must be an integer";
+		} else if (Number(max) < 0) {
+			return "The number must be positive";
+		} else if (string.length > max) {
 			string = string.slice(0, max) + "...";
 		}
-
 		
 		return string;
 	}
