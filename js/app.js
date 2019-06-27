5c5b3adf9afc9b1e03f5a3e6d27b3d3eb19bb7d1
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-27 13:38:44 +0100

    Added function for truncating a string

--- js/app.js
+++ js/app.js
@@ -3,10 +3,14 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function truncate(string, max) {
+		
+	}
+
 	let form = document.getElementById("form");
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
 
-		
+		truncate(this.children[0].value, this.children[1].value);
 	});
 };
