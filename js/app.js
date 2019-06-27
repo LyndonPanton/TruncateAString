f68fa3588de12e8372dcc5a2b476de7f75f4f80e
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-27 13:43:35 +0100

    Added logic for truncating a string

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
 	function truncate(string, max) {
+		if (string.length > max) {
+			string = string.slice(0, max) + "...";
+		}
+
 		
+		return string;
 	}
 
 	let form = document.getElementById("form");
